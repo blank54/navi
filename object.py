b44b42f857072c026135c1d307c1fa2a40484c43
SeonghyeonMoon <blank54@snu.ac.kr>
2021-05-08 23:40:38 +0900

    add MyPath

--- object.py
+++ object.py
@@ -2,6 +2,17 @@
 # -*- coding: utf-8 -*-
 
 # Configuration
+import os
+import pandas as pd
+
+from config import Config
+with open('/data/blank54/workspace/project/navi/navi.cfg', 'r') as f:
+    cfg = Config(f)
+
+
+class MyPath:
+    activity_list = os.path.join(cfg['root'], cfg['fdir_template'], 'activity_list.xlsx')
+    productivity_list = os.path.join(cfg['root'], cfg['fdir_template'], 'productivity_list.xlsx')
 
 
 class Grid:
@@ -26,18 +37,21 @@ class Grid:
         self.location_3d = tuple(self.x, self.y, self.z)
 
 
-class Activity:
+class Activity():
     '''
     A class of an individual construction activity.
 
     Attribute
     ---------
+    code : str
+        | An unique code that represents the activity.
     name : str
         | An activity name.
     productivity : int
         | The predetermined number of enable works of the activity.
     '''
 
-    def __init__(self, name, productivity):
+    def __init__(self, code, name, productivity):
+        self.code = code
         self.name = name
         self.productivity = productivity
