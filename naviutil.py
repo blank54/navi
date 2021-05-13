a3686311ddadc11a502a3484780b83407e8a7d33
SeonghyeonMoon <blank54@snu.ac.kr>
2021-05-13 22:40:30 +0900

    modify fpath template

--- naviutil.py
+++ naviutil.py
@@ -9,15 +9,14 @@ class NaviPath:
     root = '/data/blank54/workspace/project/navi/'
     
     fdir_template = os.path.join(root, 'template/')
-    fdir_objects = os.path.join(root, 'objects/')
     fdir_data = os.path.join(root, 'data/')
     
     activity_list = os.path.join(fdir_template, 'activity_list.xlsx')
     activity_sequence = os.path.join(fdir_template, 'activity_sequence.xlsx')
     productivity_list = os.path.join(fdir_template, 'productivity_list.xlsx')
 
-    activity2code = os.path.join(fdir_objects, 'activity2code.json')
-    activity2productivity = os.path.join(fdir_objects, 'activity2productivity.json')
-    activity_tree = os.path.join(fdir_objects, 'activity_tree.pk')
+    activity2code = os.path.join(fdir_template, 'activity2code.json')
+    activity2productivity = os.path.join(fdir_template, 'activity2productivity.json')
+    activity_tree = os.path.join(fdir_template, 'activity_tree.pk')
 
     case_01 = os.path.join(fdir_data, 'case_01.xlsx')
