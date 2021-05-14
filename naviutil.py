1961da79fdd6a6c2d8d0eb0474521979383c66f5
SeonghyeonMoon <blank54@snu.ac.kr>
2021-05-14 11:04:06 +0900

    modify filepath: add component

--- naviutil.py
+++ naviutil.py
@@ -9,14 +9,12 @@ class NaviPath:
     root = '/data/blank54/workspace/project/navi/'
     
     fdir_template = os.path.join(root, 'template/')
+    fdir_component = os.path.join(root, 'component')
     fdir_data = os.path.join(root, 'data/')
     
-    activity_list = os.path.join(fdir_template, 'activity_list.xlsx')
-    activity_sequence = os.path.join(fdir_template, 'activity_sequence.xlsx')
-    productivity_list = os.path.join(fdir_template, 'productivity_list.xlsx')
+    activity_table = os.path.join(fdir_template, 'activity_table.xlsx')
+    activity_order = os.path.join(fdir_template, 'activity_order.xlsx')
 
-    activity2code = os.path.join(fdir_template, 'activity2code.json')
-    activity2productivity = os.path.join(fdir_template, 'activity2productivity.json')
-    activity_tree = os.path.join(fdir_template, 'activity_tree.pk')
+    activity_tree = os.path.join(fdir_component, 'activity_tree.pk')
 
     case_01 = os.path.join(fdir_data, 'case_01.xlsx')
