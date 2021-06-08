36d5abe2198d75a6c4272fb6cd79d506a98e716b
SeonghyeonMoon <blank54@snu.ac.kr>
2021-06-08 13:07:45 +0900

    add release note: reschedule based on productivity

--- naviutil.py
+++ naviutil.py
@@ -11,6 +11,7 @@ class NaviPath:
     fdir_template = os.path.join(root, 'template/')
     fdir_component = os.path.join(root, 'component')
     fdir_data = os.path.join(root, 'data/')
+    fdir_proj = os.path.join(root, 'proj/')
 
     activity_table = os.path.join(fdir_template, 'activity_table.xlsx')
     activity_order = os.path.join(fdir_template, 'activity_order.xlsx')
@@ -18,6 +19,7 @@ class NaviPath:
     navisystem = os.path.join(fdir_component, 'navisystem.pk')
 
     case_01 = os.path.join(fdir_data, 'case_01.xlsx')
-    case_01_proj = os.path.join(fdir_data, 'case_01_proj.pk')
     case_01_schedule = os.path.join(fdir_data, 'case_01_schedule.xlsx')
     case_01_reschedule = os.path.join(fdir_data, 'case_01_reschedule.xlsx')
+
+    case_01_proj = os.path.join(fdir_proj, 'case_01.pk')
