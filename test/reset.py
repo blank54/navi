d8f22985a11be7c44a3368a7204676c515ee4d86
SeonghyeonMoon <blank54@snu.ac.kr>
2021-07-03 14:52:40 +0900

    add function: reset

--- test/reset.py
+++ test/reset.py
@@ -2,3 +2,26 @@
 # -*- coding: utf-8 -*-
 
 # Configuration
+import shutil
+
+import os
+rootpath = os.path.sep.join(os.path.dirname(os.path.abspath(__file__)).split(os.path.sep)[:-1])
+
+import sys
+sys.path.append(rootpath)
+from naviutil import NaviPath
+navipath = NaviPath()
+
+
+def do_reset():
+    reset_dirs = [navipath.fdir_component, 
+                  navipath.fdir_proj, 
+                  navipath.fdir_schedule,]
+
+    for dir in reset_dirs:
+        shutil.rmtree(dir)
+        print('Reset: {}'.format(dir))
+
+
+if __name__ == '__main__':
+    do_reset()
