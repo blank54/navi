8702cacc7d47ca7eb9ef0293b898b5fdd3e3b458
SeonghyeonMoon <blank54@snu.ac.kr>
2021-07-03 14:46:26 +0900

    support rootpath

--- test/reschedule.py
+++ test/reschedule.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # Configuration
+import os
+rootpath = os.path.sep.join(os.path.dirname(os.path.abspath(__file__)).split(os.path.sep)[:-1])
+
 import pickle as pk
 
 import sys
-sys.path.append('/data/blank54/workspace/project/navi/')
+sys.path.append(rootpath)
 from naviutil import NaviPath
 navipath = NaviPath()
 
