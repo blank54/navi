f4ce461774758b80f746585a965a77f21bee55e3
SeonghyeonMoon <blank54@snu.ac.kr>
2021-07-20 00:25:03 +0900

    add function: sort grids

--- run/reset.py
+++ run/reset.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 
 # Configuration
-import shutil
-
 import os
-rootpath = os.path.sep.join(os.path.dirname(os.path.abspath(__file__)).split(os.path.sep)[:-1])
-
 import sys
+rootpath = os.path.sep.join(os.path.dirname(os.path.abspath(__file__)).split(os.path.sep)[:-1])
 sys.path.append(rootpath)
+
 from naviutil import NaviPath
 navipath = NaviPath()
+
+import shutil
 
 
 def do_reset():
@@ -18,7 +18,7 @@ def do_reset():
                   navipath.fdir_proj, 
                   navipath.fdir_schedule,]
 
-    print('==================================================')
+    print('============================================================')
     print('RESET workspace')
     for dir in reset_dirs:
         try:
