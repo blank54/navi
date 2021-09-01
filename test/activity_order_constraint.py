0bb0136adcfaa045890b82be77f955a952d83e03
SeonghyeonMoon <blank54@snu.ac.kr>
2021-09-01 19:41:01 +0900

    reconstruct: test activity order constraint

--- test/activity_order_constraint.py
+++ test/activity_order_constraint.py
@@ -16,6 +16,13 @@ import pickle as pk
 from collections import defaultdict
 
 
+def import_schedule(fname):
+    global activit_book
+
+    schedule = navifunc.xlsx2schedule(activity_book=activity_book, fname=fname)
+    normalized_schedule = normallize_duplicated_activity(schedule)
+    return normalized_schedule
+
 def normallize_duplicated_activity(schedule):
     normalized_schedule = defaultdict(dict)
 
@@ -33,6 +40,8 @@ def normallize_duplicated_activity(schedule):
     return normalized_schedule
 
 def check_activity_order_within_work(local_schedule, activity_code):
+    global activit_book
+
     workday = [day for day in local_schedule.keys() if local_schedule[day] == activity_code][0]
 
     conflict_items = []
@@ -62,34 +71,28 @@ def reorder_activity(local_schedule, activity_code, conflict_items):
     current = [day for day in local_schedule.keys() if local_schedule[day] == activity_code][0]
     move_to = max([day for day, _ in conflict_items])
 
-    # print(current)
-    # print(move_to)
-
     for day in range(0, len(local_schedule), 1):
         if day < current or day > move_to:
-            # print('irrelevant: {}'.format(day))
             local_schedule_updated[day] = local_schedule[day]
         elif day == current:
             local_schedule_updated[move_to] = local_schedule[day]
-            # print('reorder this: {}'.format(day))
         else:
             local_schedule_updated[day-1] = local_schedule[day]
-            # print('pull these: {}'.format(day))
 
     return {d: a for d, a in sorted(local_schedule_updated.items(), key=lambda x:x[0])}
-    # for day, activity_code in sorted(local_schedule_updated.items(), key=lambda x:x[0]):
-    #     print('{:2}: {}'.format(day, activity_code))
 
-def update_order_in_local_schedule(local_schedule):
+def update_order_in_local_schedule(local_schedule, verbose=False):
     for day, activity_code in sorted(local_schedule.items(), key=lambda x:x[0]):
         conflict_items = check_activity_order_within_work(local_schedule, activity_code)
         
-        print('{:2}: {}'.format(day, activity_code))
+        if verbose:
+            print('{:2}: {}'.format(day, activity_code))
+        else:
+            pass
+
         if conflict_items:
-            print('============================================================')
-            for day, activity_code2 in conflict_items:
-                print('  | Conflicts at {}: {}'.format(day, activity_code2))
-            # print('============================================================')
+            for conflicted_day, conflicted_activity_code in conflict_items:
+                print('  | Conflicts at {}({}) <-> {}({})'.format(activity_code, day, conflicted_activity_code, conflicted_day))
 
             local_schedule_updated = deepcopy(reorder_activity(local_schedule, activity_code, conflict_items))
             return local_schedule_updated
@@ -98,27 +101,29 @@ def update_order_in_local_schedule(local_schedule):
 
     return local_schedule
 
+def update_order(schedule, verbose_iter=False, verbose_local=False):
+    schedule_updated = defaultdict(dict)
 
+    iteration = 0
+    for location in schedule.keys():
+        local_schedule_original = deepcopy(schedule[location])
+        while True:
+            iteration += 1
 
-# def check_activity_order_on_single_location(schedule, location):
-#     global activity_book
+            if verbose_iter:
+                print('============================================================')
+                print('Iteration: {:03,d} ({})'.format(iteration, location))
+            else:
+                pass
 
-#     results = []
+            local_schedule_updated = deepcopy(update_order_in_local_schedule(local_schedule=local_schedule_original, verbose=verbose_local))
+            if navifunc.compare_local_schedule(local_schedule_original, local_schedule_updated) == 'same':
+                schedule_updated[location] = deepcopy(local_schedule_updated)
+                break
+            else:
+                local_schedule_original = deepcopy(local_schedule_updated)
 
-#     for activity_code1, activity_code2 in itertools.combinations(schedule[location].values(), r=2):
-#         try:
-#             activity_1 = activity_book[activity_code1]
-#             activity_2 = activity_book[activity_code2]
-#         except KeyError:
-#             continue
-
-#         if activity_code2 in activity_1.successor:
-#             results.append('fine')
-#         elif activity_code2 in activity_1.predecessor:
-#             results.append('conflict')
-#         else:
-#             results.append('irrelevant')
-
+    return schedule_updated
 
 
 if __name__ == '__main__':
@@ -127,20 +132,6 @@ if __name__ == '__main__':
         activity_book = pk.load(f)
 
     case_num = '01'
-    fname_initial_schedule = os.path.join(navipath.fdir_schedule, 'C-{}.xlsx'.format(case_num))
-    schedule = navifunc.xlsx2schedule(activity_book=activity_book, fname=fname_initial_schedule)
-    normalized_schedule = normallize_duplicated_activity(schedule)
-
-    iteration = 0
-    local_schedule_original = deepcopy(normalized_schedule['2_1_1'])
-    while True:
-        print('Iteration: {:03,d}'.format(iteration))
-        local_schedule_updated = deepcopy(update_order_in_local_schedule(local_schedule_original))
-
-        print(local_schedule_updated)
-
-        if navifunc.compare_local_schedule(local_schedule_original, local_schedule_updated) == 'same':
-            break
-        else:
-            local_schedule_original = deepcopy(local_schedule_updated)
-            iteration += 1
+    fname_schedule_original = os.path.join(navipath.fdir_schedule, 'C-{}.xlsx'.format(case_num))
+    schedule_original = import_schedule(fname=fname_schedule_original)
+    schedule_updated = update_order(schedule=schedule_original, verbose_iter=True, verbose_local=False)
